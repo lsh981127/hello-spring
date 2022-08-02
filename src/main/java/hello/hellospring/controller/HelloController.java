70fddf65c554747e818a302507dbeec3c58e8d70
lsh981127 <lsh981127@gmail.com>
2022-08-01 22:40:32 -0500

    Hello-Spring Practice(MVC Study)

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) { // Parameter를 받을거임
+        model.addAttribute("name", name);
+        return "hello-template";
+        // hello-template 으로 랜더링하는 거야
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // 제일 중요한거 넣어줘야함
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체를 return 함
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
